414f870b92d83ebf45c2fdadb4d6d152c9e96c56
ychangkim <kkmbal@gmail.com>
2025-04-01 18:52:09 +0900

    update

--- app/common-code/page.tsx
+++ app/common-code/page.tsx
@@ -9,7 +9,6 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Checkbox } from '@/components/ui/checkbox';
 import { ConfirmDialog } from '@/components/ui/confirm-dialog';
-
 import TablePagination from "@/components/ui/table-pagination";
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
 import {
