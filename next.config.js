e8019fa44e8393106cb5c3820f8f66bf0e255558
ychangkim <kkmbal@gmail.com>
2025-04-03 23:36:36 +0900

    udpate

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL+'/:path*',
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
