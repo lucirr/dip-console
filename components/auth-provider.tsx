5f95c0218202edb2515b3240b5d419b14dd83f27
ychangkim <kkmbal@gmail.com>
2025-04-11 16:02:51 +0900

    update

--- components/auth-provider.tsx
+++ components/auth-provider.tsx
@@ -84,7 +84,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
   if (loading) {
     // return <div>Loading...</div>;
     return (
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/60 backdrop-blur-sm">
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-white backdrop-blur-sm">
         <div className="h-8 w-8 animate-spin rounded-full border-2 border-t-transparent border-gray-800" />
       </div>
     );
