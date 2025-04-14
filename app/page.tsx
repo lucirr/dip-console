13f323a715d72f8ec6c13a76817f6e4430daf7d4
ychangkim <kkmbal@gmail.com>
2025-04-14 12:54:30 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
-import { useEffect } from 'react';
 import { redirect } from 'next/navigation';
-import { useSession } from 'next-auth/react';
 
 export default function Home() {
-  const { data: session, status } = useSession();
-
-  useEffect(() => {
-    if (status === 'authenticated') {
-      redirect('/catalog');
-    }
-  }, [status]);
-
-  return null;
+  redirect('/catalog');
 }
