c1631593c6e0d4420d5aee02f25ba90dffe15f5b
ychangkim <kkmbal@gmail.com>
2025-04-15 17:07:03 +0900

    update

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -6,7 +6,7 @@ const httpsAgent = new https.Agent({
   rejectUnauthorized: false
 });
 
-const apiUrl: string = process.env.NEXT_PUBLIC_API_URL ?? '/';
+const apiUrl: string = process.env.NODE_ENV === "production" ? 'http://dip-api:8087/' : process.env.NEXT_PUBLIC_API_URL ?? '/';
 const apiAuth: string = 'api/v1';
 const token: string = 'Basic ' + btoa((process.env.NEXT_PUBLIC_DIP_API_USER ?? '') + ':' + (process.env.NEXT_PUBLIC_DIP_API_TOKEN ?? ''));
 const hostname: string = process.env.NEXT_PUBLIC_X_HOSTNAME ?? '/';
